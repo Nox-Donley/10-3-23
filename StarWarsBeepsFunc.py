523bb7be09d1c593cf154a2225df2a5036ec134c
Nox-Donley <145069348+Nox-Donley@users.noreply.github.com>
2023-10-03 15:13:40 -0600

    Add files via upload

---
+++ StarWarsBeepsFunc.py
@@ -0,0 +1,22 @@
+from winsound import Beep
+def scrumble():
+    Beep(440, 500)
+    Beep(440, 500)
+    Beep(440, 500)
+    Beep(349, 350)
+    Beep(523, 150)
+    Beep(440, 500)
+    Beep(349, 350)
+    Beep(523, 150)
+    Beep(440, 1000)
+    Beep(659, 500)
+    Beep(659, 500)
+    Beep(659, 500)
+    Beep(698, 350)
+    Beep(523, 150)
+    Beep(415, 500)
+    Beep(349, 350)
+    Beep(523, 150)
+    Beep(440, 1000)
+
+scrumble()
